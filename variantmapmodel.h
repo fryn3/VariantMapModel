eacb3be4bbfe2ad72dee054e2eb4e24956926122
karapetyan <karapetyan@inftest.ru>
2020-07-13 15:13:57 +0300

    Добавлены значения по умолчанию

--- variantmapmodel.h
+++ variantmapmodel.h
@@ -67,9 +67,9 @@ private:
 
 public:
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-    int columnCount(const QModelIndex &parent) const override;
-    QVariant data(const QModelIndex &index, int role) const override;
-    bool setData(const QModelIndex &index, const QVariant &value, int role) override;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
     Qt::ItemFlags flags(const QModelIndex &index) const override;
     QHash<int, QByteArray> roleNames() const override;
     bool removeRows(int row, int count, const QModelIndex &parent = QModelIndex()) override;
